60d862eee033fc09fbd502aa31f3e30c3a0af287
JKKIM <jkesonjk@gmail.com>
2023-07-17 23:46:27 +0900

    feat: 레이아웃 디자인 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,20 @@
 import React, { ReactNode } from 'react';
+import { styled } from 'styled-components';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return <div>{children}</div>;
+  return <LayoutContainer>{children}</LayoutContainer>;
 };
 
 export default Layout;
+
+const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #cae8ff;
+  padding: 100px 0;
+`;
