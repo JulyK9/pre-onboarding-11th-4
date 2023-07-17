7f5bd2d04762ddf02ec4318b43bd484353c10652
JKKIM <jkesonjk@gmail.com>
2023-07-17 23:47:14 +0900

    feat: title  디자인 적용

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { styled } from 'styled-components';
 
 interface TitleProps {
   firstLine: string;
@@ -7,11 +7,20 @@ interface TitleProps {
 
 const Title = ({ firstLine, secondLine }: TitleProps) => {
   return (
-    <>
+    <TitleContainer>
       <h2>{firstLine}</h2>
       <h2>{secondLine}</h2>
-    </>
+    </TitleContainer>
   );
 };
 
 export default Title;
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  font-size: 22px;
+  margin-bottom: 40px;
+`;
